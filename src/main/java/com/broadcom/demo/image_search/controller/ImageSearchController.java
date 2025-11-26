0c1f19c8c33903d4fb36361f9723c0bf1dc97e0d
Jig Sheth <jsheth@vmware.com>
2025-11-26 10:41:47 -0600

    update

--- src/main/java/com/broadcom/demo/image_search/controller/ImageSearchController.java
+++ src/main/java/com/broadcom/demo/image_search/controller/ImageSearchController.java
@@ -1,11 +1,13 @@
 package com.broadcom.demo.image_search.controller;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.springframework.ai.document.Document;
 import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +17,7 @@ import com.broadcom.demo.image_search.component.ImageMetadataLoader;
 /**
  * REST Controller to handle user search queries and retrieve image references.
  */
+@CrossOrigin(origins = "http://localhost,http://localhost:8080,http://192.168.1.148,http://192.168.1.148:8080")
 @RestController
 public class ImageSearchController {
     private final VectorStore vectorStore;
@@ -40,7 +43,7 @@ public class ImageSearchController {
      * @return A list of associated image file names.
      */
     @GetMapping("/search-images")
-    public List<String> searchImages(
+    public Map<String, Double> searchImages(
             @RequestParam(value = "query") String query,
             @RequestParam(value = "topK", defaultValue = "5") int topK) {
 
@@ -49,32 +52,37 @@ public class ImageSearchController {
         // 1. Perform a similarity search on the text embeddings
         // SearchRequest searchRequest =
         // SearchRequest.builder().query(query).topK(topK).similarityThreshold(0.7).build();
-        SearchRequest searchRequest = SearchRequest.builder().query("Represent this sentence for searching relevant passages: "+query).similarityThreshold(0.60).topK(topK).build();
+        SearchRequest searchRequest = SearchRequest.builder()
+                .query("Represent this sentence for searching relevant passages: " + query).similarityThreshold(0.60)
+                .topK(topK).build();
         List<Document> retrievedDocs = vectorStore.similaritySearch(searchRequest);
 
         // 2. Extract the associated image file name from the metadata of each retrieved
         // document
-        List<String> imageReferences = retrievedDocs.stream()
-                .map(doc -> {
+        Map<String, Double> imageReferenceScores = retrievedDocs.stream()
+                .collect(Collectors.toMap(doc -> {
                     // The key "image_file_name" must match the key used in PhotoRAGLoader
-                    Object fileName = doc.getMetadata().get("image_full_path");
+                    // Object fileName = doc.getMetadata().get("image_full_path");
+                    Object fileName = doc.getMetadata().get("image_file_name");
                     Object content = doc.getFormattedContent();
                     Double score = doc.getScore();
-                    String ctxImagePath = "";
-                    if (fileName != null) {
-                      String image_full_path = fileName.toString();
-                      int lastIndex = image_full_path.lastIndexOf('/');
-                      int secondLastIndex = image_full_path.lastIndexOf('/', lastIndex - 1);
-                      ctxImagePath = image_full_path.substring(secondLastIndex);
-                    } else
-                      return "UNKNOWN_FILE_REF";
-                    System.out.printf("   - Retrieved Document (Similarity Score %f): %s -> %s%n", score, content, ctxImagePath);
+                    // String ctxImagePath = "";
+                    // if (fileName != null) {
+                    // String image_full_path = fileName.toString();
+                    // int lastIndex = image_full_path.lastIndexOf('/');
+                    // int secondLastIndex = image_full_path.lastIndexOf('/', lastIndex - 1);
+                    // ctxImagePath = image_full_path.substring(secondLastIndex);
+                    // } else
+                    // return "UNKNOWN_FILE_REF";
+                    System.out.printf("   - Retrieved Document (Similarity Score %f): %s -> %s%n", score, content,
+                            fileName);
+                    return fileName.toString();
+                    // return ctxImagePath;
+                },
+                        doc -> doc.getScore()));
+        // .collect(Collectors.toList());
 
-                    return ctxImagePath;
-                })
-                .collect(Collectors.toList());
-
-        System.out.println("--- ✅ Retrieved " + imageReferences.size() + " image references. ---");
-        return imageReferences;
+        System.out.println("--- ✅ Retrieved " + imageReferenceScores.size() + " image references. ---");
+        return imageReferenceScores;
     }
 }
